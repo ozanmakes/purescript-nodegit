00d4da4d0f0daf60c9e2121bb398f2db5cedb451
Ozan Sener <ozan@ozansener.com>
2016-04-25 00:17:56 +0300

    Add more Commit methods

--- src/Node/Git/Commit.purs
+++ src/Node/Git/Commit.purs
@@ -1,10 +1,16 @@
 module Node.Git.Commit where
 
+import Prelude (class Show)
+
+import Data.Date (LocaleOffset, Date)
+import Data.Time (Seconds, Milliseconds)
 import Data.Function (runFn0)
-import Data.Date (Date)
 import Unsafe.Coerce (unsafeCoerce)
 
 foreign import data Commit :: *
+
+instance showCommit :: Show Commit where
+  show = unsafeCoerce \a -> runFn0 a.toString
 
 -- | Returns the commit time as a Date object.
 date :: Commit -> Date
@@ -17,3 +23,19 @@ message = unsafeCoerce \a -> runFn0 a.message
 -- | Returns the raw message of a commit
 messageRaw :: Commit -> String
 messageRaw = unsafeCoerce \a -> runFn0 a.messageRaw
+
+-- | Returns the SHA of a commit.
+sha :: Commit -> String
+sha = unsafeCoerce \a -> runFn0 a.sha
+
+-- | Returns the commit time
+time :: Commit -> Seconds
+time = unsafeCoerce \a -> runFn0 a.time
+
+-- | Returns the commit time as a unix timestamp.
+timeMs :: Commit -> Milliseconds
+timeMs = unsafeCoerce \a -> runFn0 a.timeMs
+
+-- | Returns positive or negative timezone offset, in minutes from UTC
+timeOffset :: Commit -> LocaleOffset
+timeOffset = unsafeCoerce \a -> runFn0 a.timeOffset
